d9c7c24c257ad918ed36edf6cbb2837a2da8c749
CodeEric13 <bibeh2949@gmail.com>
2025-08-03 10:04:48 +0800

    purchase filtering added

--- SAPMS/Dashboard/allPurchaseUC.Designer.cs
+++ SAPMS/Dashboard/allPurchaseUC.Designer.cs
@@ -41,9 +41,19 @@
             this.colInputVat = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colDateOfTransact = new DevExpress.XtraGrid.Columns.GridColumn();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.fltrCode = new DevExpress.XtraEditors.TextEdit();
+            this.fltrDate = new DevExpress.XtraEditors.DateEdit();
+            this.fltrSupplier = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.allPurchaseGrdCtrl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.purchaseRecordBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrCode.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrDate.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrDate.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrSupplier.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // allPurchaseGrdCtrl
@@ -52,10 +62,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.allPurchaseGrdCtrl.DataSource = this.purchaseRecordBindingSource;
-            this.allPurchaseGrdCtrl.Location = new System.Drawing.Point(0, 46);
+            this.allPurchaseGrdCtrl.Location = new System.Drawing.Point(0, 58);
             this.allPurchaseGrdCtrl.MainView = this.gridView1;
             this.allPurchaseGrdCtrl.Name = "allPurchaseGrdCtrl";
-            this.allPurchaseGrdCtrl.Size = new System.Drawing.Size(584, 248);
+            this.allPurchaseGrdCtrl.Size = new System.Drawing.Size(584, 236);
             this.allPurchaseGrdCtrl.TabIndex = 0;
             this.allPurchaseGrdCtrl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -136,16 +146,82 @@
             // 
             // simpleButton1
             // 
-            this.simpleButton1.Location = new System.Drawing.Point(3, 3);
+            this.simpleButton1.Location = new System.Drawing.Point(3, 8);
             this.simpleButton1.Name = "simpleButton1";
             this.simpleButton1.Size = new System.Drawing.Size(108, 37);
             this.simpleButton1.TabIndex = 1;
             this.simpleButton1.Text = "Preview";
             // 
+            // fltrCode
+            // 
+            this.fltrCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fltrCode.Location = new System.Drawing.Point(263, 32);
+            this.fltrCode.Name = "fltrCode";
+            this.fltrCode.Size = new System.Drawing.Size(100, 20);
+            this.fltrCode.TabIndex = 3;
+            this.fltrCode.TextChanged += new System.EventHandler(this.fltrCode_TextChanged_1);
+            // 
+            // fltrDate
+            // 
+            this.fltrDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fltrDate.EditValue = null;
+            this.fltrDate.Location = new System.Drawing.Point(479, 32);
+            this.fltrDate.Name = "fltrDate";
+            this.fltrDate.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.fltrDate.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.fltrDate.Size = new System.Drawing.Size(100, 20);
+            this.fltrDate.TabIndex = 4;
+            this.fltrDate.TextChanged += new System.EventHandler(this.fltrDate_TextChanged_1);
+            // 
+            // fltrSupplier
+            // 
+            this.fltrSupplier.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fltrSupplier.Location = new System.Drawing.Point(369, 32);
+            this.fltrSupplier.Name = "fltrSupplier";
+            this.fltrSupplier.Properties.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.fltrSupplier.Size = new System.Drawing.Size(104, 20);
+            this.fltrSupplier.TabIndex = 5;
+            this.fltrSupplier.TextChanged += new System.EventHandler(this.fltrSupplier_TextChanged);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl1.Location = new System.Drawing.Point(263, 13);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(69, 13);
+            this.labelControl1.TabIndex = 6;
+            this.labelControl1.Text = "Business Code";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl2.Location = new System.Drawing.Point(369, 13);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(38, 13);
+            this.labelControl2.TabIndex = 7;
+            this.labelControl2.Text = "Supplier";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl3.Location = new System.Drawing.Point(479, 13);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(95, 13);
+            this.labelControl3.TabIndex = 8;
+            this.labelControl3.Text = "Date of Transaction";
+            // 
             // allPurchaseUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.fltrSupplier);
+            this.Controls.Add(this.fltrDate);
+            this.Controls.Add(this.fltrCode);
             this.Controls.Add(this.simpleButton1);
             this.Controls.Add(this.allPurchaseGrdCtrl);
             this.Name = "allPurchaseUC";
@@ -154,7 +230,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.allPurchaseGrdCtrl)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.purchaseRecordBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrCode.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrDate.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrDate.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fltrSupplier.Properties)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -172,5 +253,11 @@
         private DevExpress.XtraGrid.Columns.GridColumn colNetOfVat;
         private DevExpress.XtraGrid.Columns.GridColumn colInputVat;
         private DevExpress.XtraGrid.Columns.GridColumn colDateOfTransact;
+        private DevExpress.XtraEditors.TextEdit fltrCode;
+        private DevExpress.XtraEditors.DateEdit fltrDate;
+        private DevExpress.XtraEditors.TextEdit fltrSupplier;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
     }
 }
--- SAPMS/Dashboard/allPurchaseUC.cs
+++ SAPMS/Dashboard/allPurchaseUC.cs
@@ -64,5 +64,112 @@ namespace SAPMS.Dashboard
                 MessageBox.Show($"Error loading data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+        private async void FilterPurchaseData_TextChanged(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;uid=root;pwd=;database=sapmsdb";
+
+            string query = @"
+        SELECT 
+            id,code,TinNo,supplier,grossPurchase,netOfVat,inputVat,dateOfTransac
+            
+        FROM purchase
+        WHERE (@date IS NULL OR DATE(dateOfTransac) = @date)
+          AND (@supplier IS NULL OR supplier LIKE CONCAT('%', @supplier, '%'))
+          AND (@code IS NULL OR code LIKE CONCAT('%', @code, '%'));";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    await conn.OpenAsync();
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@date", DateTime.TryParse(fltrDate.Text, out DateTime date) ? date : (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@supplier", string.IsNullOrWhiteSpace(fltrSupplier.Text) ? (object)DBNull.Value : fltrSupplier.Text.Trim());
+                        //cmd.Parameters.AddWithValue("@tinNo", int.TryParse(fltrTinNo.Text, out int tin) ? tin : (object)DBNull.Value);
+                        //cmd.Parameters.AddWithValue("@netOfVat", decimal.TryParse(fltrNetOfVat.Text, out decimal netVat) ? netVat : (object)DBNull.Value);
+                        //cmd.Parameters.AddWithValue("@inputVat", decimal.TryParse(fltrInputVat.Text, out decimal inputVat) ? inputVat : (object)DBNull.Value);
+                        //cmd.Parameters.AddWithValue("@grossPurchase", decimal.TryParse(fltrGrossPurchase.Text, out decimal gross) ? gross : (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@code", string.IsNullOrWhiteSpace(fltrCode.Text) ? (object)DBNull.Value : fltrCode.Text.Trim());
+
+                        DataTable dt = new DataTable();
+                        using (MySqlDataReader reader = (MySqlDataReader)await cmd.ExecuteReaderAsync())
+                        {
+                            dt.Load(reader);
+                        }
+
+                        allPurchaseGrdCtrl.DataSource = dt;
+                        allPurchaseGrdCtrl.MainView.PopulateColumns();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error filtering purchase data:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        //private async void FilterData_TextChanged(object sender, EventArgs e)
+        //{
+        
+        ////    string codeFilter = fltrCode.Text.Trim();
+        ////    string dateFilter = fltrDate.Text.Trim();
+        ////    string supplierFilter = fltrSupplier.Text.Trim();  // ✅ New filter
+
+        ////    string query = @"
+        ////SELECT code, customerName, VatSales, outputSales, dateOfTransac, grossSales
+        ////FROM sales
+        ////WHERE (@code IS NULL OR code LIKE CONCAT('%', @code, '%'))
+        ////  AND (@date IS NULL OR DATE(dateOfTransac) = @date)
+        ////  AND (@supplier IS NULL OR customerName LIKE CONCAT('%', @supplier, '%'));";
+
+        ////    using (MySqlConnection conn = new MySqlConnection("server=localhost;uid=root;pwd=;database=sapmsdb"))
+        ////    {
+        ////        try
+        ////        {
+        ////            await conn.OpenAsync();
+        ////            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+        ////            {
+        ////                cmd.Parameters.AddWithValue("@code", string.IsNullOrWhiteSpace(codeFilter) ? (object)DBNull.Value : codeFilter);
+
+        ////                if (DateTime.TryParse(dateFilter, out DateTime parsedDate))
+        ////                    cmd.Parameters.AddWithValue("@date", parsedDate.Date);
+        ////                else
+        ////                    cmd.Parameters.AddWithValue("@date", DBNull.Value);
+
+        ////                cmd.Parameters.AddWithValue("@supplier", string.IsNullOrWhiteSpace(supplierFilter) ? (object)DBNull.Value : supplierFilter);
+
+        ////                DataTable dt = new DataTable();
+        ////                using (MySqlDataReader reader = (MySqlDataReader)await cmd.ExecuteReaderAsync())
+        ////                {
+        ////                    dt.Load(reader);
+        ////                }
+
+        ////                allPurchaseGrdCtrl.DataSource = dt;
+        ////                allPurchaseGrdCtrl.MainView.PopulateColumns();
+        ////            }
+        ////        }
+        ////        catch (Exception ex)
+        ////        {
+        ////            MessageBox.Show($"Error filtering data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        ////        }
+        ////    }
+        //}
+
+        private void fltrCode_TextChanged_1(object sender, EventArgs e)
+        {
+            FilterPurchaseData_TextChanged(sender, e);
+        }
+
+        private void fltrDate_TextChanged_1(object sender, EventArgs e)
+        {
+            FilterPurchaseData_TextChanged(sender, e);
+        }
+
+        private void fltrSupplier_TextChanged(object sender, EventArgs e)
+        {
+            FilterPurchaseData_TextChanged(sender, e);
+        }
     }
 }
